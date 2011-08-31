8d0ddd647efa0545146f6dd02797082350abaee5
William Howard <whoward.tke@gmail.com>
2011-08-31 10:36:11 -0400

    bump patch version

--- lib/jslint-v8/version.rb
+++ lib/jslint-v8/version.rb
@@ -3,7 +3,7 @@ module JSLintV8
   module Version
      MAJOR = 1
      MINOR = 0
-     PATCH = 0
+     PATCH = 1
      BUILD = nil
     
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
